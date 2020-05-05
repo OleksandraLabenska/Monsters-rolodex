eecb611cfd3f8420e8f4e4379de1c64996426140
Oleksandra Labenska <oleksandra_labenska@hotmail.com>
2020-05-05 12:59:52 +0200

    Updates

---
+++ precache-manifest.af3cd3844a0d11d2eaabea7fac843c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d51fa69903d87e8f5ba215a3fa56e26",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "414932e10cd244625c53",
+    "url": "/Monsters-rolodex/static/css/main.1d934bd8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414932e10cd244625c53",
+    "url": "/Monsters-rolodex/static/js/main.c95f1756.chunk.js"
+  },
+  {
+    "revision": "187e6764a2c841b24bfe",
+    "url": "/Monsters-rolodex/static/js/runtime-main.8d598150.js"
+  }
+]);
